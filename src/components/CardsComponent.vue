99d19fadbd670dfeba99e5f3c78be43024b108b5
LucioGhedo <lucioghedina@gmail.com>
2022-07-15 12:45:25 +0200

    cards component

---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card-container">
+        <div class="card" v-for="item, index in CardsArray" :key="index">
+            <img :src="require(`../assets/img/${item.img}.png`)" :alt="item.text">
+            <span class="text">{{item.text}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CardsComponent',
+    props: {
+        CardsArray: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/style/common.scss';
+.card-container {
+    display: flex;
+    margin: 40px 0;
+}
+.card {
+    background-color: $back_8;
+    width: calc(99% / 5);
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0 10px;
+    border: solid $back_4 1px;
+    img {
+        width: 70px;
+        padding-bottom: 15px;
+    }
+    .text {
+        font-weight: 800;
+    }
+}
+
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,23 @@
 <template>
-    <div>ciao</div>
+    <div class="container">
+        <CardsComponent :CardsArray="cardsArray" />
+    </div>
 </template>
 
 <script>
+import mainCards from '../assets/json/mainCards.json';
+import CardsComponent from './CardsComponent.vue';
+
 export default {
     name: 'MainComponent',
+    components: {
+        CardsComponent
+    },
+    data() {
+        return {
+            cardsArray: mainCards
+        }
+    }
 }
 </script>
 
